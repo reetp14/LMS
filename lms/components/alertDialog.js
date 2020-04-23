53c698a2777a2a81a3ecfe5f64084b8e237917f5
trippi hippi <reet.p14@gmail.com>
2020-04-23 13:17:21 +0530

    added login auth

---
+++ lms/components/alertDialog.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function AlertDialog(props) {
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        onClose={() => props.closeAlert(event)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Error!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {props.message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={(event) => props.closeAlert(event)} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- lms/pages/login.js
+++ lms/pages/login.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { Paper, Button } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import AlertDialog from "../components/alertDialog";
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -41,6 +42,67 @@ const useStyle = makeStyles((theme) => ({
 
 export default function Login() {
   const classes = useStyle();
+
+  const [loginDetails, setLoginDetails] = useState({
+    emailId: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    console.log(event.target.name);
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    setLoginDetails({ ...loginDetails, [name]: value });
+  }
+
+  const [alertToggle, setAlertToggle] = useState({
+    state: false,
+    message: " ",
+  });
+
+  function handleClose() {
+    setAlertToggle({ ...alertToggle, state: false, message: "" });
+    console.log(alertToggle);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("handleSubmit");
+
+    const payload = {
+      email: loginDetails.email,
+      password: loginDetails.password,
+    };
+    console.log(payload);
+    const body = await loginReq("http://localhost:80/app/auth", payload);
+    if (body.auth === true) {
+      router.push("/dashboard");
+    } else {
+      setAlertToggle({
+        state: true,
+        message: body.errmsg,
+      });
+    }
+  }
+
+  async function loginReq(url = "", payload) {
+    // console.log(JSON.stringify(payload));
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+
+      body: JSON.stringify(payload),
+    });
+    var body = await response.json();
+    console.log(body.auth);
+    return body;
+  }
+
   return (
     <div className={classes.root}>
       <CssBaseline>
@@ -57,11 +119,11 @@ export default function Login() {
               margin="normal"
               required
               fullWidth
-              id="emailId"
+              id="email"
               label="Email Address"
-              name="emailId"
-              // value={loginDetails.emailId}
-              // onChange={handleChange}
+              name="email"
+              value={loginDetails.email}
+              onChange={handleChange}
               autoComplete="email"
               autoFocus
             />
@@ -75,14 +137,28 @@ export default function Login() {
               type="password"
               label="Password"
               name="password"
-              // value={loginDetails.emailId}
-              // onChange={handleChange}
+              value={loginDetails.password}
+              onChange={handleChange}
               autoComplete="password"
               autoFocus
             />
-            <Button variant="contained" color="primary">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(event) => {
+                console.log("clicked");
+                {
+                  handleSubmit(event);
+                }
+              }}
+            >
               Login
             </Button>
+            <AlertDialog
+              open={alertToggle.state}
+              message={alertToggle.message}
+              closeAlert={handleClose}
+            />
             <Typography variant="body1" style={{ marginTop: "10px" }}>
               New to eLMS? <Link href="/signup">Register</Link>
             </Typography>
